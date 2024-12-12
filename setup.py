bf435ad8308d0a971ebaeccf26782ad982d8f1be
irahnarak <162414568+irahnarak@users.noreply.github.com>
2024-12-13 01:20:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tamil_translite",
-    version="0.1",
+    version="0.2.4",
     description="A package for phonetic transliteration of Tamil text into English.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -10,6 +10,10 @@ setup(
     author_email="irahnarak@gmail.com",
     url="https://github.com/irahnarak/tamil_translite",
     packages=find_packages(),
+    package_data={
+        "tamil_translite": ["translit_rules.json"],  # Include the .json file
+    },
+    include_package_data=True,
     tests_require=["pytest"],
     keywords="Tamil, Transliteration, Phonetic, Tamil to English",
     classifiers=[
